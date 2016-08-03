868031cd45d4584456494e27725bd477a37f0466
K1 Fathi <k1.fathi33@gmail.com>
2016-08-02 18:28:15 -0700

    first Test 950512

---
+++ wp453en-angular.php
@@ -0,0 +1,12 @@
+<?php
+
+/*
+Plugin Name: Wp453en Angular
+Plugin URI: http://URI_Of_Page_Describing_Plugin_and_Updates
+Description: A brief description of the Plugin.
+Version: 1.0
+Author: k1
+Author URI: http://URI_Of_The_Plugin_Author
+License: A "Slug" license name e.g. GPL2
+*/
+add_action('_admin_menu',_add_themes_utility_last());
